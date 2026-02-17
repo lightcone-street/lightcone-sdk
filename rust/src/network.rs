607ea8ce2f488af576dc5f20a89a821afc003cb4
Harry Ephremsen <harryephremsen@me.com>
2026-02-17 17:07:33 +0000

    misc

--- rust/src/network.rs
+++ rust/src/network.rs
@@ -4,4 +4,4 @@
 pub const DEFAULT_API_URL: &str = "https://tapi.lightcone.xyz";
 
 /// Default WebSocket URL for Lightcone.
-pub const DEFAULT_WS_URL: &str = "wss://ws.lightcone.xyz/ws";
+pub const DEFAULT_WS_URL: &str = "wss://tws.lightcone.xyz/ws";
